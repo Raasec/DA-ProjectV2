d0f0485dc213e20b7e10de66ec7aba64f4abb890
Raasec <viraecesarosu@gmail.com>
2025-06-19 15:55:12 +0100

    Reapply "n mudei nada francamente so teste"
    This reverts commit ccabceeb656f59ae710ca175aaff6330702aa4ef.

---
+++ TipoTarefa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTasks
+{
+    internal class TipoTarefa
+    {
+        public int TipoTarefaId { get; set; }
+        public string TipoTarefaName { get; set; }
+    }
+}
